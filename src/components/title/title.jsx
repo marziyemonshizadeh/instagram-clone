1f2aac1d73f0e669a3d65d985721865898b6a2e1
marzieh <marziehmonshizade@yahoo.com>
2023-07-31 02:15:33 +0330

    Add a beautiful style to the text titles of the About me page

---
+++ src/components/title/title.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./title.css";
+
+function title({ children }) {
+  return <h3 className="my-4 titles d-block fw-bold mt-5 fs-5">{children}</h3>;
+}
+
+export default title;
