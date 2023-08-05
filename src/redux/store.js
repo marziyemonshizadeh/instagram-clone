0b1ff24f99876cb456d5f78c4fda82732df0794c
marzieh <marziehmonshizade@yahoo.com>
2023-08-05 19:01:18 +0330

    Fix the middleware default error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
-import commentsReducer from '../redux/store/posts/comments';
-import postsReducer from '../redux/store/posts/posts';
-import storiesReducer from '../redux/store/story/story';
-import productsReducer from './store/shopping/products';
+import commentsReducer from "../redux/store/posts/comments";
+import postsReducer from "../redux/store/posts/posts";
+import storiesReducer from "../redux/store/story/story";
+import productsReducer from "./store/shopping/products";
 // import storeReducer from '../redux/storeResourse/store.js';
 
-export default configureStore( {
-      reducer:{
-        posts : postsReducer,
-        comments : commentsReducer,
-        products : productsReducer,
-        stories : storiesReducer,
-      }
-})
-
+export default configureStore({
+  reducer: {
+    posts: postsReducer,
+    comments: commentsReducer,
+    products: productsReducer,
+    stories: storiesReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
