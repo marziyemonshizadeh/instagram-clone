a5fc01e0cfe758852722355f0e4e06f579a720a3
marzieh <marziehmonshizade@yahoo.com>
2023-08-12 02:18:12 +0330

    Customize profile page

--- src/Pages/profile/profile.jsx
+++ src/Pages/profile/profile.jsx
@@ -3,39 +3,32 @@ import React from "react";
 export default function Profile() {
   return (
     <div className="d-flex justify-content-center mt-5">
-      <div className="card p-5" style={{ width: 800 }}>
-        <svg
+      <div
+        className="card p-5"
+        style={{ width: 800, backgroundColor: "#f9fcfec5" }}
+      >
+        <img
+          src="https://images.squarespace-cdn.com/content/v1/54b7b93ce4b0a3e130d5d232/1519987020970-8IQ7F6Z61LLBCX85A65S/icon.png?format=1000w"
+          alt="nn"
+          width={150}
+          height={150}
           className="mx-auto mb-3 bd-placeholder-img rounded-circle"
-          width="140"
-          height="140"
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-label="Placeholder"
-          preserveAspectRatio="xMidYMid slice"
-          focusable="false"
-        >
-          <title>Placeholder</title>
-          <rect
-            width="100%"
-            height="100%"
-            fill="var(--bs-secondary-color)"
-          ></rect>
-        </svg>
-        <div className="d-flex justify-content-between p-3 border-bottom border-dark-subtle">
+        />
+        <div className="d-md-flex justify-content-between p-3 border-bottom border-dark-subtle">
           <p className="fw-bold text-muted">USERNAME</p>
-          <p>Marzieh</p>
+          <p style={{ color: "#F22390" }}>Marzieh</p>
         </div>
-        <div className="d-flex justify-content-between p-3 border-bottom border-dark-subtle">
+        <div className="d-md-flex justify-content-between p-3 border-bottom border-dark-subtle">
           <p className="fw-bold text-muted">FULLNAME</p>
-          <p>marziehkarimi</p>
+          <p style={{ color: "#F22390" }}>marziehkarimi</p>
         </div>
-        <div className="d-flex justify-content-between p-3 border-bottom border-dark-subtle">
+        <div className="d-md-flex justify-content-between p-3 border-bottom border-dark-subtle">
           <p className="fw-bold text-muted">EMAIL</p>
-          <p>marzieh@yahoo.com</p>
+          <p style={{ color: "#F22390" }}>marzieh@yahoo.com</p>
         </div>
-        <div className="d-flex justify-content-between p-3 border-bottom border-dark-subtle">
+        <div className="d-md-flex justify-content-between p-3 border-bottom border-dark-subtle">
           <p className="fw-bold text-muted">PASSWORD</p>
-          <p>123456789</p>
+          <p style={{ color: "#F22390" }}>123456789</p>
         </div>
       </div>
     </div>
