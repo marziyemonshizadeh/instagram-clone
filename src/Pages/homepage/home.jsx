71bbb093d32119d7896108a81828e1e009c8ebbd
marzieh <marziehmonshizade@yahoo.com>
2023-08-14 20:42:47 +0330

    Show the new post first

--- src/Pages/homepage/home.jsx
+++ src/Pages/homepage/home.jsx
@@ -1,24 +1,23 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Posts from '../../components/instagramPosts/post';
-import Story from '../../components/story/story';
-import { getPostsFromServer } from '../../redux/store/posts/posts';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Posts from "../../components/Posts/post";
+import Story from "../../components/story/story";
+import { getPostsFromServer } from "../../redux/store/posts/posts";
 
 export default function Home() {
-    const posts = useSelector((state) => state.posts)
-    const dispatch = useDispatch();
-    console.log('post:',posts);
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+  console.log("post:", posts);
 
-    useEffect(() => {
-        dispatch(getPostsFromServer("/posts"))
-    },[])
-    return (
-      <>
-          <Story/>
-          {posts.map((post)=>(
-                <Posts key={post.id} {...post} />
-              ))
-          } 
-      </>
-    )
+  useEffect(() => {
+    dispatch(getPostsFromServer("/posts?_sort=id&_order=desc"));
+  }, []);
+  return (
+    <>
+      <Story />
+      {posts.map((post) => (
+        <Posts key={post.id} {...post} />
+      ))}
+    </>
+  );
 }
