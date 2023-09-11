2ec6e5d88ea61d2104a0fcb381518658a94aac1f
marzieh <marziehmonshizade@yahoo.com>
2023-09-11 15:08:03 +0330

    deploy json-server on liara

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("mydb.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(3001, () => {
+  console.log("JSON Server is running marziii");
+});
