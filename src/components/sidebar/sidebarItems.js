199e0b09190174d424e70b2e5de2d8be09f3ed03
marzieh <marziehmonshizade@yahoo.com>
2023-10-01 18:46:40 +0330

    Remove the likes page from the sidebar

--- src/components/sidebar/sidebarItems.js
+++ src/components/sidebar/sidebarItems.js
@@ -1,7 +1,7 @@
 import { BiAddToQueue, BiStore } from "react-icons/bi";
 import { BsBoxArrowRight, BsCart3, BsInfoCircle } from "react-icons/bs";
 import { CgProfile } from "react-icons/cg";
-import { FaRegHeart } from "react-icons/fa";
+// import { FaRegHeart } from "react-icons/fa";
 import { MdHomeFilled } from "react-icons/md";
 import { RiBookmarkLine } from "react-icons/ri";
 
@@ -11,7 +11,7 @@ let sidebarItems = [
   { icon: BiAddToQueue, text: "Create", to: "/main/create" },
   { icon: BsInfoCircle, text: "AboutUs", to: "/main/aboutus" },
   { icon: RiBookmarkLine, text: "Saves", to: "/main/saves" },
-  { icon: FaRegHeart, text: "Likes", to: "/main/likes" },
+  // { icon: FaRegHeart, text: "Likes", to: "/main/likes" },
   { icon: CgProfile, text: "Profile", to: "/main/profile" },
   { icon: BsCart3, text: "Cart", to: "/main/cart" },
   { icon: BsBoxArrowRight, text: "Log out", to: "/" },
